81574ee4dd89ec837bc126fd7b0ace005ddf80a1
Kay Kim <kimjh@bawi.org>
2022-01-25 01:32:14 +0900

    hotfix: Revert viewport meta, disable CSP

--- next.config.js
+++ next.config.js
@@ -1,51 +1,10 @@
-const { StatusCodes } = require('http-status-codes');
-// @ts-check
 const withPWA = require('next-pwa');
 const runtimeCaching = require('next-pwa/cache');
-
-const securityHeaders = [
-  {
-    key: 'X-XSS-Protection',
-    value: '1; mode=block',
-  },
-  {
-    key: 'Content-Security-Policy',
-    value: [
-      `default-src 'self'`,
-      `base-uri 'self'`,
-      `style-src 'self' 'unsafe-inline'`,
-      `font-src 'self'`,
-      `worker-src 'self' data: blob:`,
-      `script-src 'self' www.googletagmanager.com 'sha256-TXxkhhv5PbMCjHYmlg9z/7BJZyFLLrozKCYjRZVuuHg=' 'unsafe-eval'`,
-      `object-src 'self'`,
-      `img-src 'self' data: blob:`,
-      `connect-src 'self' www.google-analytics.com analytics.google.com ws:`,
-    ].join(';'),
-  },
-];
 
 /**
  * @type {import('next').NextConfig}
  **/
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: securityHeaders,
-      },
-    ];
-  },
-  async redirects() {
-    return [
-      {
-        source: '/docs',
-        destination: '/docs/index.html',
-        statusCode: StatusCodes.PERMANENT_REDIRECT,
-        permanent: true,
-      },
-    ];
-  },
   pwa: { dest: 'public' },
   runtimeCaching,
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -36,14 +36,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <DefaultSeo
         title="Kay's Image Resizer"
         description="super-fast image resizer web application"
-        additionalMetaTags={[
-          {
-            name: 'viewport',
-            content:
-              'width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no',
-          },
-          { name: 'theme-color', content: '#ffffff' },
-        ]}
+        additionalMetaTags={[{ name: 'theme-color', content: '#ffffff' }]}
         additionalLinkTags={[
           {
             rel: 'icon',
